29ab0c15d4f7305ce29a19dcb9f788400748978e
valadaa48 <valadaa48@gmx.com>
2020-03-10 03:36:43 +0000

    Changing the default key scan sampling rate to 100Hz from 500Hz

--- dist/opt/retroroller/scripts/retroroller_boot.sh
+++ dist/opt/retroroller/scripts/retroroller_boot.sh
@@ -12,3 +12,7 @@ chmod g+w /sys/devices/platform/dmc/devfreq/dmc/governor
 echo -n 0 > /sys/devices/platform/gpio_leds/leds/blue\:heartbeat/brightness
 
 #sed -i 's/#HandlePowerKey=poweroff/HandlePowerKey=ignore/' /etc/systemd/logind.conf
+
+# The bounce noise seems to be input without filtering due to the too fast sampling rate.
+# Changing the default key scan sampling rate to 100Hz from 500Hz could remove most of double registering problems.
+echo 10 > /sys/devices/platform/odroidgo2-joypad/poll_interval
